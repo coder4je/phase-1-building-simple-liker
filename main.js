147f3ad522ef262dc804a0c8ea5cd2bae9d69a52
Taemin Kim <taeminkimny@gmail.com>
2022-05-26 23:20:07 -0400

    commit tree 95e4d0feac8b682e69f32839e4582c4222ecad2b

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector('#modal')
+const modalH2 = document.querySelector('#modal h2')
+const heart = document.querySelectorAll('.like-glyph')
+let modal2 = document.querySelector('.hidden')
 
 // Your JavaScript code goes here!
 
+modal.className = 'hidden';
 
+  
 
+heart.forEach(ht => {
+  ht.addEventListener('click', () => {
+    if (EMPTY_HEART) {
+      mimicServerCall()
+      .then(res => {
+        console.log(res);
+      })
+  
+      .catch((error) => {
+        console.log(error)
+        modal.hidden = false;
+        modalH2.textContent = 'error';
+      })
+      setTimeout(() => {
+        modal.hidden = true;  
+        ht.textContent = FULL_HEART;  
+      }, 3000);
+  
+      ht.classList.add('activated-heart');
+    }
+  
+    else {
+      ht.textContent = EMPTY_HEART;
+      ht.classList.remove('activated-heart');
+    }
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
